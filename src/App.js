bb3764aa652460295514d86edbcace561827624c
mxteroy <mxtery@gmail.com>
2020-01-25 17:17:04 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MapContainer from './map.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MapContainer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
